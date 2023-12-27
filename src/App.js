51ae758907f9f52dab0c4d0182da94ad489cf19a
Trần Đạt <tranxuandat.dev@gmail.com>
2023-12-27 16:03:14 +0700

    update
    - useReducer
    - axios

--- src/App.js
+++ src/App.js
@@ -1,21 +1,54 @@
-import { useContext, useRef, useState } from "react";
+import { useContext, useEffect, useRef, useState } from "react";
+import { useReducer } from "react";
+import { BrowserRouter,Route, Link } from "react-router-dom";
 import "./App.css";
 import Footer from "./components/Footer";
 import Header from "./components/Header";
 import TodoList from "./components/TodoList";
 import Toggle from "./components/Toggle";
 import { themeContext } from "./components/ThemeProvider";
-import { todoData } from "./components/TodoData";
 import TodolistTest from "./components/TodolistTest";
+import axios from 'axios';
+import Donate from "./components/Donate";
 // =======================================================
 export const filterStatus = {
   all: "all",
   done: "done",
   notDone: "notDone"
 };
+function todoReducer(arr, action){
+  switch(action.type){
+    case 'add':{
+      return [
+        {
+          id: new Date().valueOf(),
+          text: action.text,
+          isDone: false,
+        },
+        ...arr,
+      ];
+    }
+    case 'delete':{
+      return arr.filter(item => item.id !== action.id);
+    }
+    case 'update':{
+      return arr.map((todo) => {
+        if (todo.id === action.id) {
+          return { ...todo, id: action.id,text:action.text };
+        }
+        return todo ;
+      });
+    }
+    case 'checkbox':{
+      return arr.map((todo)=>
+        todo.id===action.id?{...todo, isDone: !todo.isDone}:todo);
+    }
+  }
+}
+let res = await axios.get('https://6588fac4324d4171525855f8.mockapi.io/api/todos');
 
 const App = () => {
-  const [arr, setArr] = useState(todoData);
+  const [arr, dispatch] = useReducer(todoReducer, res.data);
   const [scroll, setScroll]= useState(false);
   const {theme} = useContext(themeContext);
   const headerRef = useRef(null);
@@ -23,7 +56,13 @@ const App = () => {
   const setTodoStatus = (value)=>{
     setStatus(value);
   }
-  
+  // useEffect(() => {
+  //   axios.get('https://6588fac4324d4171525855f8.mockapi.io/api/todos')
+  //     .then(res => {
+  //       console.log(res)
+  //       return res.data;
+  //     })
+  // }, []);
 //filter
   const filterByStatus = (arr, status) => {
     switch (status) {
@@ -37,41 +76,6 @@ const App = () => {
   };
   const filterItemLeft = (arr) => {
     return arr.filter((item) => !item.isDone);
-  };
-  const filterItem = (arr, id) => {
-    return arr.filter((item) => item.id !== id);
-  };
-  //addTodo
-  const addTodo = (text) => {
-    setArr([
-      {
-        id: new Date().valueOf(),
-        text,
-        isDone: false,
-      },
-      ...arr,
-    ]);
-  };
-  //delTodo
-  const delTodo = (id) => {
-    setArr(filterItem(arr, id));
-  };
-  //update
-  const updateTodo = (id, text) => {
-    const newArr=arr.map((todo) => {
-      if (todo.id == id) {
-        return { ...todo, id: id,text:text };
-      }
-      return todo ;
-    });
-    setArr(newArr)
-  };
-  //check box
-  const clickCheckBox=(item)=>{
-    const cbArr = arr.map((todo)=>
-    todo.id===item.id?{...todo, isDone: !todo.isDone}:todo);
-    setArr(cbArr);
-    console.log(item.id)
   }
   //focus input
   const editTodoHeader = (id, text)=>{
@@ -83,18 +87,44 @@ const App = () => {
   const toggleScroll= ()=>{
     setScroll(scroll==false?true:false)
   }
+  function addTodo(text){
+    dispatch({
+      type:'add',
+      text:text,
+    })
+  }
+  function delTodo(id){
+    dispatch({
+      type:'delete',
+      id:id,
+    })
+  }
+  function updateTodo(id,text){
+    dispatch({
+      type:'update',
+      id:id,
+      text:text,
+    })
+  }
+  function clickCheckBox(item){
+    dispatch({
+      type:'checkbox',
+      id:item.id,
+    })
+  }
+  
   return (
-    <div className={"w-3/4 !h-[610px] bg-red-300 mx-auto " + theme}>
+      <div className={"w-3/4 !h-[610px] bg-red-300 mx-auto " + theme}>
       <h1 className="text-center text-8xl text-rose-400 font-bold">TODOS</h1>
-      <Header addTodo={addTodo} ref={headerRef} updateTodo={updateTodo} />
+      <Header ref={headerRef} addTodo={addTodo} updateTodo={updateTodo} />
       <Toggle toggleScroll={toggleScroll} />
       <TodoList
         arr={filterByStatus(arr, status)}
-        delTodo={delTodo}
-        updateTodo={updateTodo}
         editTodoHeader={editTodoHeader}
-        clickCheckBox={clickCheckBox}
         scroll={scroll}
+        delTodo={delTodo}
+        clickCheckBox={clickCheckBox}
+        updateTodo={updateTodo}
       />
       {/* <TodolistTest arr={filterByStatus(arr,status)}/> */}
       <Footer
